ae6a97278c149809bb153551eed59b4df73ce2ed
4triumph <864195969@qq.com>
2024-05-27 21:20:19 +0800

    main

---
+++ 283.move-zeroes.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=cpp
+ * @lcpr version=30202
+ *
+ * [283] 移动零
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int len = (int)nums.size();
+        int l = 0, r = 0;
+        while(r < len){
+            if(nums[r]){
+                swap(nums[l], nums[r]);
+                l ++ ;
+                
+            }
+            r ++ ;
+        }
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [0,1,0,3,12]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n
+// @lcpr case=end
+
+ */
+
